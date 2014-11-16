60a83711c9fcd41c74aa45c042f1f93b62e86beb
William Bain <bain.william.a@gmail.com>
2014-11-16 01:43:50 -0500

    Redo build system
    - Move to Assemble to build HTML
    - Move towards unification of the dev and release build processes
    - Inline the analytics initialization
    - Move initially towards a multi-page static site, although there
      will be some JS-based single-page navigation added in future
    - Consolidate static assets under assets/ (although it's just
      /img so far) and move sections/ to src/content/ (and change to
      templates).
    - Check in a shrinkwrapped node modules listing

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,21 +8,58 @@ module.exports = function (grunt) {
             '<%= grunt.option("release") ? "" : "[dev build]" %>, ' +
             '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
             '   Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>',
+
         // Task configuration
-        clean: {
-            dist: ['./dist/**', '!dist'],
-            dev: ['tmp-dev/**']
+        assemble: {
+            options: {
+                layout: 'page.hbs',
+                layoutdir: 'src/scaffolding/layouts/',
+                partials: 'src/scaffolding/partials/**/*.hbs',
+                assets: 'dist/home-assets',
+                helpers: ['prettify'],
+                prettify: {
+                    indent: 2,
+                    condense: true,
+                    padcomments: true
+                }
+            },
+            pages: {
+                options: {
+                    pkg: '<%= pkg %>',
+                    banner: "<%= pkg.name %> v<%= pkg.version %> <%= grunt.option('release') ? '' : '[dev build]' %>, <%= grunt.template.today('yyyy-mm-dd') %>\n   Copyright (c) <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>",
+                    release: '<%= grunt.option("assemble-release") %>'
+                },
+                cwd: 'src/content',
+                dest: 'dist/',
+                expand: true,
+                src: '**/*.hbs'
+            },
+            'section-partials': {
+                options: {
+                    layout: 'section-partial.hbs'
+                },
+                cwd: 'src/content',
+                dest: 'dist/section-partial/',
+                expand: true,
+                src: '**/*.hbs'
+            }
         },
+
+        clean: {
+            dist: ['dist/**', '!dist']
+        },
+
         concat: {
             options: {
                 banner: '/* <%= banner %> */\n',
                 stripBanners: true
             },
-            dist: {
+            js: {
                 src: ['src/js/**/*.js'],
                 dest: 'dist/home-assets/cs-homepage.js'
             }
         },
+
         uglify: {
             options: {
                 banner: '/* <%= banner %> */',
@@ -30,161 +67,117 @@ module.exports = function (grunt) {
                 // Default is to remove the .js
                 sourceMapName: function (name) {return name+'.map';}
             },
-            dist: {
-                src: '<%= concat.dist.dest %>',
+            default: {
+                src: 'dist/home-assets/cs-homepage.js',
                 dest: 'dist/home-assets/cs-homepage.min.js'
             }
         },
+
         copy: {
-            distScss: {
+            sass: {
                 options: {
                     // Prepend the banner
                     process: function (content) {
-                        return '/* '+grunt.config('banner')+' */'+'\n'+content;
+                        return '/* '+grunt.config('banner')+' */\n'+content;
                     }
                 },
                 files: [
                     {
                         expand: true,
                         cwd: 'src/scss/',
-                        src: 'cs-homepage.scss',
+                        src: ['**/*.scss'],
                         dest: 'dist/home-assets/'
                     }
                 ]
             },
-            distVendor: {
+            other_assets: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'assets',
+                        src: ['img/**/*'],
+                        dest: 'dist/home-assets/'
+                    }
+                ]
+            },
+            bower_assets: {
                 files: [
                     {
                         expand: true,
                         cwd: 'bower_components/html5shiv/dist/',
                         src: 'html5shiv.min.js',
-                        dest: 'dist/home-assets/'
-                    }
+                        dest: 'dist/home-assets/vendor/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/fancybox/source/',
+                        src: '**/*',
+                        dest: 'dist/home-assets/vendor/fancybox/'
+                    },
                 ]
             }
         },
+
         /* Sass loses out on compression a bit relative to cssmin (color names,
            0px vs 0) but it does source maps and it's good enough */
         sass: {
-            dist: {
+            release: {
                 options: {
-                    style: 'compressed',
-                    sourcemap: true
+                    style: 'compressed'
                 },
                 src: 'dist/home-assets/cs-homepage.scss',
                 dest: 'dist/home-assets/cs-homepage.min.css'
             },
             dev: {
                 options: {
-                    style: 'expanded',
-                    sourcemap: true
+                    style: 'expanded'
                 },
-                src: 'src/scss/cs-homepage.scss',
-                dest: 'tmp-dev/cs-homepage.css'
+                src: 'dist/home-assets/cs-homepage.scss',
+                dest: 'dist/home-assets/cs-homepage.css'
             }
         },
-        template: {
-            dist: {
-                options: {
-                    data: {
-                        index_banner: '<%= banner.replace(/\\n\\s*/, "\\n     ") %>',
-                        version: '<%= pkg.version %>',
-                        cookieDomain: '<%= grunt.option("release") ? "cs.mcgill.ca" : "none" %>',
-                        cookiePath: '<%= grunt.option("release") ? "/~wbain" : "/" %>',
-                        sections: {
-                            about: 'sections/about.html',
-                            projects: 'sections/projects.html',
-                            cv: 'sections/cv.html'
-                        },
-                        scripts: {
-                            html5shiv: 'home-assets/html5shiv.min.js',
-                            jquery: '//code.jquery.com/jquery-1.11.0.min.js',
-                            enhancement: 'home-assets/cs-homepage.min.js',
-                            // enhancement: '<%= uglify.dist.dest %>'
-                            analytics: 'src/inline/init-analytics.js'
-                        },
-                        styles: {
-                            page: 'home-assets/cs-homepage.min.css'
-                            // page: '<%= sass.dist.dest %>'
-                        },
-                        readFile: readFile
-                    }
-                },
-                src: 'src/index.html',
-                dest: 'dist/index.html'
-            },
-            dev: {
-                options: {
-                    data: {
-                        index_banner: 'Local dev build at <%= grunt.template.today("yyyy-mm-dd hh:MM") %>',
-                        version: '<%= pkg.version %>',
-                        cookieDomain: 'none',
-                        cookiePath: '/',
-                        sections: {
-                            about: 'sections/about.html',
-                            projects: 'sections/projects.html',
-                            cv: 'sections/cv.html'
-                        },
-                        scripts: {
-                            html5shiv: '../bower_components/html5shiv/dist/html5shiv.js',
-                            jquery: '../bower_components/jquery/dist/jquery.js',
-                            enhancement: '../src/js/home-navigation.js',
-                            analytics: '<%= grunt.option("dev-analytics") ? "src/inline/init-analytics.js" : null %>'
-                        },
-                        styles: {
-                            page: 'cs-homepage.css'
-                        },
-                        readFile: readFile
-                    }
-                },
-                src: 'src/index.html',
-                dest: 'tmp-dev/index.html'
-            }
-        },
-        jsbeautifier: {
-            options: {
-                html: {
-                    indentSize: 2,
-                    indentScripts: 'keep'
-                }
-            },
-            dist: {
-                src: ['dist/index.html']
-            },
-            dev: {
-                src: ['tmp-dev/index.html']
-            }
-        },
+
         watch: {
             options: {
                 spawn: true
             },
-            devScss: {
+            sass: {
                 files: ['src/scss/**/*.scss'],
                 tasks: ['sass:dev']
             },
-            devIndex: {
-                files: ['src/index.html', 'sections/**'],
-                tasks: ['template:dev']
+            html: {
+                files: ['src/scaffolding/**/*', 'src/content/**/*'],
+                tasks: ['assemble']
             },
-            jshint: {
-                files: ['gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-                tasks: ['jshint']
+            js: {
+                files: ['src/js/**/*.js'],
+                tasks: ['jshint:src', 'concat']
+            },
+            nodestuff: {
+                files: ['gruntfile.js', 'test/**/*.js'],
+                tasks: ['jshint:nodestuff']
             }
         },
+
         concurrent: {
             options: {
                 logConcurrentOutput: true
             },
-            dev: ['watch:devScss', 'watch:devIndex', 'watch:jshint']
+            dev: ['watch:sass', 'watch:html', 'watch:js', 'watch:nodestuff']
         },
+
         'http-server': {
-            dev: {
-                port: 8282,
-                host: "127.0.0.1",
-                runInBackground: true
+            default: {
+              port: 8282,
+              host: "127.0.0.1"
+            },
+            async: {
+              port: 8282,
+              host: "127.0.0.1",
+              runInBackground: true
             }
         },
+
         jshint: {
             options: {
                 node: true,
@@ -206,13 +199,14 @@ module.exports = function (grunt) {
                 },
                 boss: true
             },
-            gruntfile: {
-                src: 'gruntfile.js'
+            nodestuff: {
+                src: ['gruntfile.js', 'test/**/*.js']
             },
-            src_test: {
-                src: ['src/**/*.js', 'test/**/*.js']
+            src: {
+                src: 'src/**/*.js'
             }
         },
+
         // Not yet implemented
         qunit: {
             files: ['test/**/*.html']
@@ -225,20 +219,50 @@ module.exports = function (grunt) {
             grunt.loadNpmTasks(task);
         }
     }
+    grunt.loadNpmTasks('assemble');
 
     // High-level tasks
-    grunt.registerTask('default', ['jshint', 'clean:dist', 'concat',
-            'uglify', 'copy:distVendor', 'copy:distScss', 'sass:dist',
-            'template:dist', 'jsbeautifier:dist']);
+    grunt.registerTask('default', 'Build and run', function (buildMode) {
+        buildMode = verifyBuildMode(buildMode);
 
-    grunt.registerTask('build-dev', ['jshint', 'clean:dev', 'sass:dev',
-            'template:dev', 'jsbeautifier:dev']);
+        if (grunt.option('watch')) {
+            grunt.task.run('build:'+buildMode, 'http-server:async', 'concurrent:'+buildMode);
+        } else {
+            grunt.task.run('build:'+buildMode, 'http-server');
+        }
+    });
 
-    grunt.registerTask('watch-dev', ['concurrent:dev']);
+    grunt.registerTask('build', 'Build the site', function (buildMode) {
+        var tasks;
 
-    grunt.registerTask('run-dev', ['build-dev', 'http-server:dev', 'watch-dev']);
+        buildMode = verifyBuildMode(buildMode);
 
-    function readFile(fname) {
-        return grunt.file.read(fname).replace(/\s*$/, '');
+        if (buildMode === 'release') {
+            grunt.option('assemble-release', true);
+        }
+
+        tasks = [
+            'jshint',
+            'clean:dist',
+            'copy:sass',
+            'sass:'+buildMode,
+            'concat:js'
+        ];
+
+        if (buildMode === 'release') {
+            tasks.push('uglify', 'copy:bower_assets');
+        }
+
+        tasks.push('copy:other_assets', 'assemble');
+
+        grunt.task.run.apply(grunt.task, tasks);
+    });
+
+    function verifyBuildMode(buildMode) {
+        buildMode = buildMode || 'dev';
+        if (buildMode !== 'dev' && buildMode !== 'release') {
+            grunt.warn('unexpected build mode "'+buildMode+'"');
+        }
+        return buildMode;
     }
 };
