63884d63008975c87a26020f7f8072470a63fff1
William Bain <bain.william.a@gmail.com>
2015-10-10 12:34:53 -0400

    Fixed: Remove redundant output manipulation

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,9 @@
 
 set -e # exit with nonzero exit code if anything fails
 
-# clear and re-create the out directory
-rm -rf dist || exit 0;
-mkdir dist;
-
 npm run compile
 
-# go to the out directory and create a *new* Git repo
+# go to the output directory and create a *new* Git repo
 cd dist
 git init
 
