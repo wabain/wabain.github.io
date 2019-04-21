d162b5d6680f76fe469f29bbd11e318d3c2a9107
William Bain <bain.william.a@gmail.com>
2019-04-21 14:58:40 -0400

    Refactor: Use a Jekyll plugin to generate partials

---
+++ content/_plugins/partials.rb
@@ -0,0 +1,61 @@
+module Partials
+  class PartialGenerator < Jekyll::Generator
+    safe true
+    priority :lowest
+
+    def generate(site)
+      partials = Jekyll::Collection.new(site, "section-partial")
+      partials.metadata["output"] = true
+
+      site.pages.each do |page|
+        fpath = File.join(partials.directory, page.dir, page.name)
+
+        Jekyll.logger.debug "Generating partial:", fpath
+        partials.docs << PartialDocument.new(page, fpath, {
+          :site => partials.site,
+          :collection => partials
+        })
+      end
+
+      site.collections.each do |name, collection|
+        collection.docs.each do |doc|
+          # For posts, this can give paths like _section-partial/_posts/... or
+          # _section-partial/_drafts/..., but that doesn't seem to cause any harm
+          fpath = File.join(partials.directory, doc.relative_path)
+
+          Jekyll.logger.debug "Generating partial:", fpath
+          partials.docs << PartialDocument.new(doc, fpath, {
+            :site => partials.site,
+            :collection => partials
+          })
+        end
+      end
+
+      site.collections["section-partial"] = partials
+    end
+  end
+
+  class PartialDocument < Jekyll::Document
+    def initialize(target, path, relations = {})
+      super(path, relations)
+
+      if target.is_a? Jekyll::Page
+        @data = target.data.clone
+        self.content = target.content
+      elsif target.is_a? Jekyll::Document
+        @data = target.data.clone
+        self.content = target.content
+      else
+        raise ArgumentError, "unexpected partial target #{target}"
+      end
+
+      @url = "/section-partial" + target.url
+
+      partial_layout = self.data["partial_layout"]
+      if partial_layout == nil
+        raise ArgumentError, "no 'partial_layout' key provided for #{path}"
+      end
+      self.data["layout"] = partial_layout
+    end
+  end
+end
