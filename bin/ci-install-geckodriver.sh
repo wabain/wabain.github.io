4d360034265af4628aa9b618505a792991455315
William Bain <bain.william.a@gmail.com>
2019-05-20 19:24:22 -0400

    WIP: CI: Install geckodriver

---
+++ bin/ci-install-geckodriver.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+json=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest)
+echo "Latest geckodriver release..."
+
+echo
+echo "$json"
+url=$(echo "$json" | jq -r '.assets[].browser_download_url | select(contains("linux64"))')
+echo
+
+echo "Downloading from URL $url"
+curl -s -L "$url" | tar -xz
+chmod +x geckodriver
+mv geckodriver "$USER_INSTALL_DIR"
+echo "Installed geckodriver binary in $USER_INSTALL_DIR"
