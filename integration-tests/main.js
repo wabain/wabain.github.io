e011b7dba9f854b11ec5ee6127ba7f137548ed65
William Bain <bain.william.a@gmail.com>
2020-04-27 19:27:33 -0400

    Tests: Load site meta synchronously from local FS

--- integration-tests/main.js
+++ integration-tests/main.js
@@ -4,7 +4,9 @@
 
 const path = require('path')
 const glob = require('glob')
-const request = require('request')
+
+const env = getTestEnvironment()
+const siteMeta = require(env.siteMetaPath)
 
 glob('**/*.test.js', { cwd: __dirname }, (err, files) => {
     if (err) {
@@ -12,54 +14,31 @@ glob('**/*.test.js', { cwd: __dirname }, (err, files) => {
     }
 
     const testModules = files.map((f) => require(path.resolve(__dirname, f)))
-    const env = getTestEnvironment()
 
-    loadSiteMeta(env.siteMetaUrl)
-        .then(async (siteMeta) => {
-            for (const main of testModules) {
-                if (main) {
-                    const args = Object.assign({}, env, { siteMeta })
-                    await main(args)
-                }
-            }
-
-            run()
-        })
-        .catch((err) => {
-            console.error(`Test launch failed: ${err}`)
-        })
+    runMainTests(testModules).catch((err) => {
+        console.error(`Test launch failed: ${err}`)
+        process.exit(1)
+    })
 })
+
+async function runMainTests(testModules) {
+    for (const main of testModules) {
+        if (main) {
+            const args = Object.assign({}, env, { siteMeta })
+            await main(args)
+        }
+    }
+
+    run()
+}
 
 function getTestEnvironment() {
     const origin = process.env.TEST_ORIGIN || 'http://127.0.0.1:8080'
     const browser = process.env.TEST_BROWSER || 'firefox'
-    const siteMetaUrl =
-        process.env.TEST_SITE_META_URL || origin + '/.test-meta.json'
-    return { origin, browser, siteMetaUrl }
-}
 
-function loadSiteMeta(siteMetaUrl) {
-    return new Promise((res, rej) => {
-        request(siteMetaUrl, (err, resp, body) => {
-            if (err) {
-                rej(err)
-                return
-            }
+    const siteMetaPath =
+        process.env.TEST_SITE_META_PATH ||
+        path.resolve(__dirname, '../_site/.test-meta.json')
 
-            if (resp.statusCode !== 200) {
-                rej(
-                    new Error(
-                        `Unexpected status ${resp.statusCode} for ${siteMetaUrl}`,
-                    ),
-                )
-                return
-            }
-
-            try {
-                res(JSON.parse(body))
-            } catch (e) {
-                rej(e)
-            }
-        })
-    })
+    return { origin, browser, siteMetaPath }
 }
