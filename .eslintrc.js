f9684dd09dffc233afb642883ae637f2085220f6
William Bain <bain.william.a@gmail.com>
2020-12-05 22:59:26 -0500

    Build(dev-deps): Upgrade eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,6 @@ module.exports = {
             plugins: ['@typescript-eslint'],
             extends: [
                 'eslint:recommended',
-                'plugin:@typescript-eslint/eslint-recommended',
                 'plugin:@typescript-eslint/recommended',
                 'prettier/@typescript-eslint',
             ],
